3d1f5e595a2bbb712c503649a45a59f5634880d3
nithya-kala <nithyakala1988@gmail.com>
2022-12-09 10:13:06 +0530

    example to use the library

---
+++ sample/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import "./App.css"
+import useWhyRender from "../../src/index"
+
+function App(props: any) {
+  const [count, setCount] = useState(0)
+  useWhyRender(props, { count })
+
+  return (
+    <div className="App">
+      <h1>Welcome to {props.name}</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Verify the <code>web console</code> to identify why the component is
+          re-rendered.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
